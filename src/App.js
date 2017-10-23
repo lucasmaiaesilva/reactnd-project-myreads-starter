d7328344de9596bdac2400afa393e8b720dbf9ed
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2017-10-23 12:32:16 -0200

    Adding stateless component on app.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import Search from './Components/Search'
 import BookTable from './Components/BookTable'
 import './App.css'
 
-class BooksApp extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <Route path="/" component={BookTable} exact />
-        <Route path="/search" component={Search} />
-      </div>
-    )
-  }
-}
+const BooksApp = () => (
+  <div className="app">
+    <Route path="/" component={BookTable} exact />
+    <Route path="/search" component={Search} />
+  </div>
+)
 
 export default BooksApp
